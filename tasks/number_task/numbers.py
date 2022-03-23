a3dd2e079116f40787c5872017a6abd63d1500bd
Sourcery AI <>
2022-03-23 05:12:40 +0000

    'Refactored by Sourcery'

--- tasks/number_task/numbers.py
+++ tasks/number_task/numbers.py
@@ -7,39 +7,26 @@ parser.add_argument('number', help='number', type=int)
 def delete_file(file_path):
     try:
         os.remove(file_path)
-        print("Successfully deleted file: " + file_path)
+        print(f"Successfully deleted file: {file_path}")
     except OSError:
-        print("File not found: " + file_path)
-        pass
+        print(f"File not found: {file_path}")
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
     number = args.number
     print("Printing all numbers in given range")
-    f_numbers = open("/shared-volume/numbers.txt", "a")
-
-    # Copy from even.txt to numbers.txt
-    f_even = open("/shared-volume/even.txt", "r")
-    for line in f_even:
-        f_numbers.write(line)
-    f_even.close()
-
-    # Copy from odd.txt to numbers.txt
-    f_odd = open("/shared-volume/odd.txt", "r")
-    for line in f_odd:
-        f_numbers.write(line)
-    f_odd.close()
-
-    f_numbers.close()
-
-    # Print contents of numbers.txt
-    f_numbers = open("/shared-volume/numbers.txt", "r")
-    for line in f_numbers:
-        print(line)
-        print("\n")
-    f_numbers.close()
-
+    with open("/shared-volume/numbers.txt", "a") as f_numbers:
+        with open("/shared-volume/even.txt", "r") as f_even:
+            for line in f_even:
+                f_numbers.write(line)
+        with open("/shared-volume/odd.txt", "r") as f_odd:
+            for line in f_odd:
+                f_numbers.write(line)
+    with open("/shared-volume/numbers.txt", "r") as f_numbers:
+        for line in f_numbers:
+            print(line)
+            print("\n")
     # Deleting all files, to avoid EFS cost
     delete_file("/shared-volume/even.txt")
     delete_file("/shared-volume/odd.txt")
